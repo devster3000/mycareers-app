cdc8863067f1358ba72880f14a9e5363ba55632e
William Cranston <willcranston09@proton.me>
2026-01-06 16:38:55 +0000

    TopAppBar.kt now is only for the UI of the Top Bar. MainActivity.kt manages the routing and navigation.

--- app/src/main/java/com/shcg/mycareers/MainActivity.kt
+++ app/src/main/java/com/shcg/mycareers/MainActivity.kt
@@ -4,19 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavType
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import com.shcg.mycareers.ui.components.TopAppBar
 import com.shcg.mycareers.ui.screens.course.CourseScreen
 import com.shcg.mycareers.ui.screens.home.HomeScreen
 import com.shcg.mycareers.ui.screens.profile.ProfileScreen
 import com.shcg.mycareers.ui.screens.settings.SettingsScreen
 import com.shcg.mycareers.ui.screens.skills.SkillsScreen
-import com.shcg.mycareers.ui.theme.MyCareersTheme
 
 
 object Routes {
@@ -39,26 +40,31 @@ fun MyCareers() {
     val nav = rememberNavController()
 
     MaterialTheme {
-        NavHost(navController = nav, startDestination = Routes.Home) {
-
-            composable(Routes.Home) {
-                HomeScreen()
-            }
-
-            composable(Routes.Courses) {
-                CourseScreen()
-            }
-
-            composable(Routes.Skills) {
-                SkillsScreen()
-            }
-
-            composable(Routes.Profile) {
-                ProfileScreen()
-            }
-
-            composable (Routes.Settings) {
-                SettingsScreen()
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = "",
+                    onProfileClick = { nav.navigate(Routes.Profile) },
+                    onSettingsClick = { nav.navigate(Routes.Settings) }
+                )
+            },
+        ) { innerPadding ->
+            NavHost(navController = nav, startDestination = Routes.Home, modifier = Modifier.padding(innerPadding)) {
+                composable(Routes.Home) {
+                    HomeScreen()
+                }
+                composable(Routes.Courses) {
+                    CourseScreen()
+                }
+                composable(Routes.Skills) {
+                    SkillsScreen()
+                }
+                composable(Routes.Profile) {
+                    ProfileScreen()
+                }
+                composable(Routes.Settings) {
+                    SettingsScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/shcg/mycareers/ui/components/TopAppBar.kt
+++ app/src/main/java/com/shcg/mycareers/ui/components/TopAppBar.kt
@@ -1,74 +1,47 @@
 package com.shcg.mycareers.ui.components
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.runtime.getValue
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.ui.NavigationUI.navigateUp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 
 
 @Composable
-fun TopAppBar() {
+fun TopAppBar(title: Any, onProfileClick: () -> Unit, onSettingsClick: () -> Unit) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-    val navController = rememberNavController()
 
-    Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(
-                        "  ",
-                        maxLines = 1,
-                    )
-                },
+     CenterAlignedTopAppBar(
+                title = {},
                 navigationIcon = {
-                    IconButton(onClick = { navController.navigate("profile")}) {
+                    IconButton(onClick = onProfileClick) {
                         Icon(
                             imageVector = Icons.Outlined.AccountCircle,
-                            contentDescription = "Localized description"
+                            contentDescription = "Profile"
                         )
                     }
                 },
                 actions = {
-                    IconButton(onClick = { navController.navigate("settings") }) {
+                    IconButton(onClick = onSettingsClick) {
                         Icon(
                             imageVector = Icons.Outlined.Settings,
-                            contentDescription = "Localized description"
+                            contentDescription = "Settings"
                         )
                     }
                 },
-                scrollBehavior = scrollBehavior,
             )
-        },
-    ) { innerPadding -> /* TODO */}
-}
+        }
